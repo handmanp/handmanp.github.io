0c19239c02e648b12b24c71ac298e678224c5606
handmanp <windowsshu@gmail.com>
2019-11-25 13:59:35 +0900

    Update connection.js

--- script/connection.js
+++ script/connection.js
@@ -1,7 +1,8 @@
 // ---- use socket.io ----
 "use strict";
 let port = 7001;
-let socket = io.connect('http://localhost:' + port + '/');
+
+let socket = io.connect('https://handmanp.ddns.net:' + port, { secure: true });
 let room = getRoomname();
 let joiningRoom;
 let ids = {};
